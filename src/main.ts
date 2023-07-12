e18969952bd26eb2139612736446e1e9cc10f145
koreanddinghwan <myunghwan0421@gmail.com>
2023-07-12 19:40:37 +0900

    feat: enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: '*',
+    origin: `http://localhost:${process.env.REACT_PORT}`,
   });
   await app.listen(process.env.NEST_PORT);
 }
