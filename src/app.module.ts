5803e29e82b6f4f4c01a7f1d8df08fd4a00eb697
koreanddinghwan <myunghwan0421@gmail.com>
2023-07-12 16:09:51 +0900

    feat: module monolithic

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { ChatGateway } from './chat/chat.gateway';
+import { GameGateway } from './game/game.gateway';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthModule],
+  controllers: [],
+  providers: [ChatGateway, GameGateway],
 })
 export class AppModule {}
