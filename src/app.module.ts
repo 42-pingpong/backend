b6cd25e9610e79023564c4e5201ae8ab38a09a27
koreanddinghwan <myunghwan0421@gmail.com>
2023-07-12 17:12:54 +0900

    feat: typeorm module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { ChatGateway } from './chat/chat.gateway';
 import { GameGateway } from './game/game.gateway';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'postgres',
+      port: 5432,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      entities: [],
+      synchronize: true,
+    }),
+    AuthModule,
+  ],
   controllers: [],
   providers: [ChatGateway, GameGateway],
 })
