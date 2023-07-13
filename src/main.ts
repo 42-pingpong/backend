454a8947efde16122689542ee0e833b1dab15499
koreanddinghwan <myunghwan0421@gmail.com>
2023-07-13 21:16:30 +0900

    feat: swagger init

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //CORS
   app.enableCors({
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
   });
+  //Cookie
   app.use(cookieParser(process.env.COOKIE_SECRET));
+  //Global Prefix
   app.setGlobalPrefix('api');
+
+  //Swagger
+  const config = new DocumentBuilder()
+    .setTitle('42PingPong API')
+    .setDescription('42PingPong API description')
+    .setVersion('1.0')
+    .addTag('42PingPong')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.NEST_PORT);
 }
 bootstrap();
