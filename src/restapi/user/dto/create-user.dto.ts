fa15e8054da74874fce457e4dc40fa5b5cb2c3d6
myukang <myunghwan0421@gmail.com>
2023-07-19 16:30:30 +0900

    feat: user dto

--- src/restapi/user/dto/create-user.dto.ts
+++ src/restapi/user/dto/create-user.dto.ts
@@ -1,1 +1,38 @@
-export class CreateUserDto {}
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateUserDto {
+  @ApiProperty({
+    description: 'User Id(intra)',
+    example: 1,
+    required: true,
+  })
+  id: number;
+
+  @ApiProperty({
+    description: 'User Name',
+    example: 'myukang',
+    required: true,
+  })
+  nickName: string;
+
+  @ApiProperty({
+    description: 'User Profile Image',
+    example: 'https://cdn.intra.42.fr/users/myukang.jpg',
+    required: true,
+  })
+  profile: string;
+
+  @ApiProperty({
+    description: 'User Self Introduction',
+    example: 'Hello, I am myukang',
+    required: true,
+  })
+  selfIntroduction: string;
+
+  @ApiProperty({
+    description: 'User Level',
+    example: 1.0,
+    required: true,
+  })
+  level: number;
+}
