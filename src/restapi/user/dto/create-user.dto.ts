ae98acf78fb751859949cd0a34a2a6beccf8f6ae
myukang <myunghwan0421@gmail.com>
2023-07-23 03:01:31 +0900

    feat: create user dto에도 추가

--- src/restapi/user/dto/create-user.dto.ts
+++ src/restapi/user/dto/create-user.dto.ts
@@ -11,28 +11,30 @@ export class CreateUserDto {
   @ApiProperty({
     description: 'User Name',
     example: 'myukang',
-    required: true,
   })
   nickName: string;
 
   @ApiProperty({
+    description: 'User Email',
+    example: 'myunghwan0421@gmail.com',
+  })
+  email: string;
+
+  @ApiProperty({
     description: 'User Profile Image',
     example: 'https://cdn.intra.42.fr/users/myukang.jpg',
-    required: true,
   })
   profile: string;
 
   @ApiProperty({
     description: 'User Self Introduction',
     example: 'Hello, I am myukang',
-    required: true,
   })
   selfIntroduction: string;
 
   @ApiProperty({
     description: 'User Level',
     example: 1.0,
-    required: true,
   })
   level: number;
 }
