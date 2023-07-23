6f3139231461ce6200e934eef1d66adcf6d9cb55
myukang <myunghwan0421@gmail.com>
2023-07-24 04:03:27 +0900

    feat: auth Controller

--- src/restapi/auth/auth.controller.ts
+++ src/restapi/auth/auth.controller.ts
@@ -1,50 +1,52 @@
-import { Controller, Post, Body, UseGuards } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards, Param } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { CreateAuthDto } from './dto/create-auth.dto';
 import { Get, Req, Res } from '@nestjs/common';
 import { Request, Response } from 'express';
 import { AuthGuard } from '@nestjs/passport';
+import { JwtService } from '@nestjs/jwt';
+import { ApiOperation } from '@nestjs/swagger';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
-
-  @Post()
-  create(@Body() createAuthDto: CreateAuthDto) {
-    return this.authService.create(createAuthDto);
-  }
+  constructor(
+    private readonly authService: AuthService,
+    private readonly jwtService: JwtService,
+  ) {}
 
   //need auth guard
-  @Get('42/login')
+  @ApiOperation({ summary: '42 login' })
   @UseGuards(AuthGuard('42'))
+  @Get('42/login')
   async login42() {
     return null;
   }
 
-  @Get('42/redirect')
   @UseGuards(AuthGuard('42'))
+  @Get('42/redirect')
   async redirect42(
     @Req() req: Request,
     @Res({ passthrough: true }) res: Response,
   ) {
-    console.log('profile:', req.user);
-    if (!req.user) {
-      res.statusCode = 401;
-      res.send('Authorization failed');
-    } else {
-      res.statusCode = 200;
-      // database에서 req.user.id를 이용해서 유저정보를 가져와.
-      // const dbUser = await this.authService.findUser(req.user.id);
+    const rtn = await this.authService.login(req.user);
+    res.cookie('accessToken', rtn.accessToken, {
+      //this expires is checked by browser
+      expires: new Date(Date.now() + 1000 * 30),
+    });
+    res.cookie('refreshToken', rtn.refreshToken, {
+      httpOnly: true,
+      //this expires is checked by browser
+      expires: new Date(Date.now() + 1000 * 60),
+    });
+    res.redirect('http://localhost:3000/login');
+  }
 
-      // if (dbUser) {
-      //dbUser의 정보를 가지고 jwt token을 만들어서 발급
-      // }
-      // else {
-      //   //db에 req.user (최초 로그인한 유저) 정보를 저장하고
-      //   jwt 토큰을 발급한다.
-      // }
-
-      res.send('ttt');
-    }
+  @ApiOperation({ summary: 'refresh token end point' })
+  @UseGuards(AuthGuard('jwt-refresh')) //access token strategy는 AuthGuard('jwt')로 대체
+  @Get('refresh')
+  async refresh(
+    @Req() req: Request,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    console.log(res);
   }
 }
