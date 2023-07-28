b498194e878477664c3bc0e830580661800a4dc9
myukang <myunghwan0421@gmail.com>
2023-07-28 09:49:52 +0900

    feat: add bull queue module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { StatusModule } from './status/status.module';
 import { RestapiModule } from './restapi/restapi.module';
 import { appDatabase } from './datasource/appdatabase';
 import { AppConfigModule } from './config/app.config';
+import { QueueModule } from './queue/queue.module';
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import { AppConfigModule } from './config/app.config';
     GameModule,
     StatusModule,
     RestapiModule,
+    QueueModule,
   ],
 })
 export class AppModule {}
