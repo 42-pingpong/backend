f31e550dc57472131072c78391b059e16c6aeb02
myukang <myunghwan0421@gmail.com>
2023-07-29 22:16:58 +0900

    feat: add queue config

--- src/config/url.ts
+++ src/config/url.ts
@@ -2,12 +2,10 @@ import { registerAs } from '@nestjs/config';
 
 export default registerAs('url', () => ({
   frontHost:
-    process.env.NODE_ENV === 'development' ||
-    process.env.NODE_ENV === 'test' ||
-    process.env.NODE_ENV === 'e2e'
+    process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test'
       ? 'http://localhost'
-      : process.env.REACT_HOST,
-  frontPort: process.env.REACT_PORT,
+      : process.env.REACT_APP_HOST,
+  frontPort: process.env.REACT_APP_PORT,
 
   testUrl:
     process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test'
