63699f43e6693ad96b7990ac94fbbbbf3ea0cbd5
myukang <myunghwan0421@gmail.com>
2023-07-30 17:21:53 +0900

    feat: add friend/ get friend list

---
+++ src/restapi/user/dto/get-friend-query.dto.ts
@@ -0,0 +1,34 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { Transform } from 'class-transformer';
+import {
+  IsBooleanString,
+  IsEnum,
+  IsOptional,
+  ValidationError,
+} from 'class-validator';
+
+export class GetFriendQueryDto {
+  @ApiProperty({
+    description: '친구의 상태',
+    example: 'online',
+    enum: ['offline', 'online', 'inGame'],
+  })
+  @IsEnum(['offline', 'online', 'inGame'])
+  @IsOptional()
+  status?: string;
+
+  @ApiProperty({
+    description: '자신의 데이터 포함 여부',
+    example: 'true',
+    type: 'string',
+    enum: ['true', 'false'],
+  })
+  @IsOptional()
+  @Transform(({ value }) => {
+    if (value === 'true') return true;
+    else if (value === 'false') return false;
+    else throw new ValidationError();
+    return value;
+  })
+  includeMe?: boolean;
+}
