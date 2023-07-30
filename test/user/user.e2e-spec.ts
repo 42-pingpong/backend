37d784a91f0ac8e2e6007b655de9067c0860c125
myukang <myunghwan0421@gmail.com>
2023-07-30 17:22:07 +0900

    test: user e2e

--- test/user/user.e2e-spec.ts
+++ test/user/user.e2e-spec.ts
@@ -13,12 +13,15 @@ import { UserFactory } from './user.factory';
 import { AppConfigModule } from 'src/config/app.config';
 import { TestConfigModule } from 'src/config/test.config';
 import { FriendsWith } from 'src/entities/user/friendsWith.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Token } from 'src/entities/auth/token.entity';
 
 describe('User -/user (e2e)', () => {
   let app: INestApplication;
   let dataSource: DataSource;
   let repository: Repository<User>;
   let friendRepository: Repository<FriendsWith>;
+  let tokenRepository: Repository<Token>;
   const factory: UserFactory = new UserFactory();
 
   beforeAll(async () => {
@@ -29,6 +32,8 @@ describe('User -/user (e2e)', () => {
       .useModule(testDatabase)
       .overrideModule(AppConfigModule)
       .useModule(TestConfigModule)
+      .overrideModule(TypeOrmModule)
+      .useModule(TypeOrmModule.forFeature([User, FriendsWith, Token]))
       .compile();
 
     app = moduleFixture.createNestApplication();
@@ -36,6 +41,7 @@ describe('User -/user (e2e)', () => {
 
     dataSource = moduleFixture.get<DataSource>(DataSource);
     repository = dataSource.getRepository(User);
+    tokenRepository = dataSource.getRepository(Token);
     friendRepository = dataSource.getRepository(FriendsWith);
   });
 
@@ -107,23 +113,29 @@ describe('User -/user (e2e)', () => {
     });
 
     describe('GET /me/friends/{id}', () => {
+      const createUser7 = factory.createUser(7);
+      createUser7.status = 'online';
+      const createUser8 = factory.createUser(8);
+      createUser8.status = 'inGame';
+      const createUser9 = factory.createUser(9);
+      createUser9.status = 'offline';
+      const createUser10 = factory.createUser(10);
+
       let user7;
       let user8;
       let user9;
       let user10;
 
       beforeAll(async () => {
-        user7 = await repository.save(factory.createUser(7));
-        user8 = await repository.save(factory.createUser(8));
-        user9 = await repository.save(factory.createUser(9));
-        user10 = await repository.save(factory.createUser(10));
-      });
+        await friendRepository.delete({ userId: 7 });
+        await friendRepository.delete({ userId: 8 });
+        await friendRepository.delete({ userId: 9 });
+        await friendRepository.delete({ userId: 10 });
 
-      afterAll(async () => {
-        await repository.delete(user7);
-        await repository.delete(user8);
-        await repository.delete(user9);
-        await repository.delete(user10);
+        user7 = await repository.save(createUser7);
+        user8 = await repository.save(createUser8);
+        user9 = await repository.save(createUser9);
+        user10 = await repository.save(createUser10);
       });
 
       it('GET /me/friends/{id} success', async () => {
@@ -137,6 +149,11 @@ describe('User -/user (e2e)', () => {
           friendId: user9.id,
         });
 
+        await friendRepository.save({
+          userId: user7.id,
+          friendId: user10.id,
+        });
+
         const res = await request(app.getHttpServer())
           .get('/user/me/friends/7')
           .expect(200);
@@ -145,11 +162,87 @@ describe('User -/user (e2e)', () => {
         expect(res.body[1].friendId).toEqual(user9.id);
       });
     });
+
+    describe('POST /me/friends/{id}', () => {
+      let user11 = factory.createUser(11);
+      let user12 = factory.createUser(12);
+      user12.status = 'online';
+      let user13 = factory.createUser(13);
+      user13.status = 'inGame';
+      let user14 = factory.createUser(14);
+      user14.status = 'offline';
+
+      beforeAll(async () => {
+        const realIds = [107112, 106987, 106982, 106930];
+
+        await friendRepository.delete({ userId: 11 });
+        await friendRepository.delete({ userId: 12 });
+        await friendRepository.delete({ userId: 13 });
+        await friendRepository.delete({ userId: 14 });
+
+        user11 = await repository.save(user11);
+        user12 = await repository.save(user12);
+        user13 = await repository.save(user13);
+        user14 = await repository.save(user14);
+
+        for (const id of realIds) {
+          const realUser = await repository.findOne({ where: { id: id } });
+          if (realUser) {
+            await friendRepository.save({
+              userId: id,
+              friendId: user11.id,
+            });
+
+            await friendRepository.save({
+              userId: id,
+              friendId: user12.id,
+            });
+
+            await friendRepository.save({
+              userId: id,
+              friendId: user13.id,
+            });
+
+            await friendRepository.save({
+              userId: id,
+              friendId: user14.id,
+            });
+          }
+        }
+      });
+
+      it('success', async () => {
+        const res = await request(app.getHttpServer())
+          .post('/user/me/friends/11')
+          .send({
+            friendId: user12.id,
+          })
+          .expect(201);
+
+        const res2 = await request(app.getHttpServer())
+          .post('/user/me/friends/11')
+          .send({
+            friendId: user13.id,
+          })
+          .expect(201);
+
+        const res3 = await request(app.getHttpServer())
+          .post('/user/me/friends/11')
+          .send({
+            friendId: user14.id,
+          });
+
+        const friends = await friendRepository.find({
+          where: {
+            userId: 11,
+          },
+        });
+        expect(friends.length).toEqual(3);
+      });
+    });
   });
 
   afterAll(async () => {
-    await repository.delete(user1);
-    await repository.delete(user2);
     await dataSource.destroy();
     await app.close();
   });
