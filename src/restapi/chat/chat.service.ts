015c30fe3ea47a47df34421a71acd7588f0dd09b
박규민 <gyumpark@gyumpark-ui-MacBookAir.local>
2023-08-01 15:07:55 +0900

    update

--- src/restapi/chat/chat.service.ts
+++ src/restapi/chat/chat.service.ts
@@ -1,7 +1,7 @@
 import { ForbiddenException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { GroupChat } from 'src/entities/chat/groupChat.entity';
-import { Repository } from 'typeorm';
+import { EntityManager, Repository } from 'typeorm';
 import { CreateGroupChatDto } from './dto/create-group-chat.dto';
 import { UpdateGroupChatDto } from './dto/update-group-chat.dto';
 import { ExceptionsHandler } from '@nestjs/core/exceptions/exceptions-handler';
@@ -12,7 +12,6 @@ export class ChatService {
     @InjectRepository(GroupChat)
     private readonly groupChatRepository: Repository<GroupChat>,
   ) {}
-  private messages: any[] = []; // 메모리 상에서 간단히 채팅 메시지를 저장하는 예제이므로, 실제 프로젝트에서는 데이터베이스 등을 사용합니다.
 
   async getGroupChat(groupChatId: number) {
     return await this.groupChatRepository.findOne({
@@ -39,30 +38,51 @@ export class ChatService {
     /**
      * 요청한 유저가 admin/owner인지 확인하는 로직이 필요합니다.
      */
-    await this.groupChatRepository.manager.transaction(async (manager) => {
-      const curGroupChat = await manager.findOne(GroupChat, {
-        where: { groupChatId: groupChatId },
-        select: ['curParticipants'],
-      });
+    await this.groupChatRepository.manager.transaction(
+      async (manager: EntityManager) => {
+        // 그룹 채팅방의 현재 참여 인원 조회
+        const curGroupChat = await manager.findOne(GroupChat, {
+          where: { groupChatId },
+          select: ['curParticipants'],
+        });
 
-      if (curGroupChat.curParticipants >= updateGroupChatDto.maxParticipants) {
-        throw new ForbiddenException();
-      }
+        if (
+          curGroupChat.curParticipants >= updateGroupChatDto.maxParticipants
+        ) {
+          throw new ForbiddenException();
+        }
 
-      await manager.update(
-        GroupChat,
-        { groupChatId: groupChatId },
-        updateGroupChatDto,
-      );
-    });
+        await manager.update(GroupChat, groupChatId, updateGroupChatDto);
+      },
+    );
   }
 
   async addAdmin(groupChatId: number, adminId: number) {
     // 그룹 채팅방에 admin을 추가하는 로직
     // 미완성
-    // 분기문으로 인가된 유저인지 확인하는 로직 필요
-    await this.groupChatRepository.save({
-      groupChatId: groupChatId,
+
+    await this.groupChatRepository.manager.transaction(async (manager) => {
+      //1. 그룹 안의 어드민 정보를 뽑아내는 로직
+      const groupChatAdmin = await this.groupChatRepository.find({
+        where: {
+          groupChatId: groupChatId,
+        },
+        relations: {
+          admin: true,
+          owner: true,
+        },
+        select: {
+          ownerId: true,
+          admin: true,
+          // group안의 admin을 뽑을 때 어떻게 뽑아야할지 모르겠어여
+        },
+      });
+      // if (groupChatAdmin.find((admin) => admin.ownerId !== adminId)) {
+      //   throw new ForbiddenException();
+      // }
+      // 분기문으로 인가된 유저인지 확인하는 로직 필요
+
+      await this.groupChatRepository.manager.save(adminId);
     });
   }
 
@@ -70,24 +90,18 @@ export class ChatService {
     // 그룹 채팅방에서 admin을 삭제하는 로직
     // 미완성
     // 분기문으로 인가된 유저인지 확인하는 로직 필요
+    // 삭제하려는 id가 admin에 있는지 확인하는 로직 필요
     await this.groupChatRepository.delete({
       groupChatId: groupChatId,
     });
   }
-
-  sendMessage(messageData: any) {
-    // 새로운 채팅 메시지를 생성하고 저장하는 로직
-    const newMessage = {
-      id: Date.now().toString(),
-      text: messageData.text,
-      user: messageData.user,
-    };
-    this.messages.push(newMessage);
-    return newMessage;
-  }
-
-  getAllMessages() {
-    // 저장된 모든 채팅 메시지를 반환하는 로직
-    return this.messages;
-  }
 }
+
+/**
+ * 1. setPriv
+ * 2. setPub
+ * 3. saveDmChat
+ * 4. saveGroupChat
+ * 5. getDmChat
+ * 6. getGroupChat
+ */
