1c414f5f9a999732c39eefc51c56332749e01c39
Jina Kim <jinkim2@c4r3s4.42seoul.kr>
2023-08-07 21:06:08 +0900

    feat: join

--- src/sockets/game/game.gateway.ts
+++ src/sockets/game/game.gateway.ts
@@ -1,7 +1,64 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server } from 'socket.io';
 
-@WebSocketGateway()
-export class GameGateway {
+const waitList = [];
+
+@WebSocketGateway({
+  namespace: 'game',
+  cors: {
+    origin: '*',
+  },
+  transports: ['websocket'],
+})
+export class GameGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  @WebSocketServer()
+  server: Server;
+
+  afterInit(server: any) {
+    console.log('Init');
+  }
+
+  @SubscribeMessage('connect')
+  handleConnection(client: any, ...args: any[]) {
+    console.log('Connection');
+  }
+
+  @SubscribeMessage('login')
+  handleLogin(client: any, payload: any) {
+    if (waitList.includes(client)) {
+      waitList.splice(waitList.indexOf(client), 1);
+      return;
+    }
+
+    waitList.push(client);
+
+    if (waitList.length === 2) {
+      const roomName = waitList[0].id + '/' + waitList[1].id;
+      // client.join(roomName);
+      // waitList[0].join(roomName);
+      // waitList[1].join(roomName);
+      waitList[0].emit('join', roomName);
+      waitList[1].emit('join', roomName);
+      waitList[0].emit('player-number', 1);
+      waitList[1].emit('player-number', 2);
+      waitList.splice(0, 2);
+    }
+  }
+
+  @SubscribeMessage('disconnect')
+  handleDisconnect(client: any) {
+    console.log('Disconnect');
+  }
+
   @SubscribeMessage('message')
   handleMessage(client: any, payload: any): string {
     return 'Hello world!';
