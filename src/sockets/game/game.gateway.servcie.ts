6cbec4c2dc23e0bb06a185d932ec2232f4dc5277
박규민 <gyumpark@gyumpark-ui-MacBookAir.local>
2023-08-09 14:56:24 +0900

    test

--- src/sockets/game/game.gateway.servcie.ts
+++ src/sockets/game/game.gateway.servcie.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
-export class GameGatewayService {}
+export class GameGatewayService {
+  constructor(private readonly jwtService: JwtService) {}
+
+  async getNickname(): Promise<string> {
+    return 'nickname';
+  }
+}
