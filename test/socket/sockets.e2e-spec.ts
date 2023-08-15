c00ba27b4039eb081ce921590e804ad45d35c84d
myukang <myunghwan0421@gmail.com>
2023-08-15 22:04:53 +0900

    :white_check_mark: test: ban User

--- test/socket/sockets.e2e-spec.ts
+++ test/socket/sockets.e2e-spec.ts
@@ -3,7 +3,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AppConfigModule } from 'src/config/app.config';
 import { TestConfigModule } from 'src/config/test.config';
 import { testDatabase } from 'src/datasource/testDatabase';
-import { DataSource } from 'typeorm';
+import { DataSource, In } from 'typeorm';
 import { io } from 'socket.io-client';
 import { appDatabase } from 'src/datasource/appdatabase';
 import { StatusModule } from 'src/sockets/status/status.module';
@@ -17,6 +17,7 @@ import { KickUserDto } from '../../src/sockets/chat/request/kickUser.dto';
 import { GroupChat } from '../../src/entities/chat/groupChat.entity';
 import { UserFactory } from '../../src/factory/user.factory';
 import { ChatFactory } from '../../src/factory/chat.factory';
+import { BanDto } from '../../src/sockets/chat/request/ban.dto';
 
 /**
  * @link https://medium.com/@tozwierz/testing-socket-io-with-jest-on-backend-node-js-f71f7ec7010f
@@ -30,11 +31,7 @@ describe('Socket', () => {
   let ChatSocketClient;
   let GameSocketClient;
   let accToken;
-  let user10000: User;
-  let user10001: User;
-  let user10002: User;
-  let user10003: User;
-  let groupChat10000: GroupChat;
+
   const userFactory = new UserFactory();
   const chatFactory = new ChatFactory();
 
@@ -81,52 +78,9 @@ describe('Socket', () => {
 
     await restApp.listen(10050);
     datasource = moduleFixture2.get<DataSource>(DataSource);
-
-    user10000 = await datasource
-      .getRepository(User)
-      .save(userFactory.createUser(10000));
-
-    user10001 = await datasource
-      .getRepository(User)
-      .save(userFactory.createUser(10001));
-    user10002 = await datasource
-      .getRepository(User)
-      .save(userFactory.createUser(10002));
-    user10003 = await datasource
-      .getRepository(User)
-      .save(userFactory.createUser(10003));
-    groupChat10000 = await datasource
-      .getRepository(GroupChat)
-      .save(chatFactory.createPubChat(user10000.id, 10001));
-    await datasource
-      .createQueryBuilder(GroupChat, 'groupChat')
-      .relation('admin')
-      .of(groupChat10000)
-      .add(user10001);
-    await datasource
-      .createQueryBuilder(GroupChat, 'groupChat')
-      .relation('joinedUser')
-      .of(groupChat10000)
-      .add([user10002, user10003]);
   });
 
   beforeEach(async () => {
-    // Setup
-    //10000번 유저로 로그인
-    const res = await request(restApp.getHttpServer())
-      .get('/api/fakeauth/login')
-      .query({
-        id: user10000.id,
-      });
-
-    accToken = res.headers['location'].split('accessToken=')[1];
-
-    //login
-    await request(restApp.getHttpServer())
-      .get('/api/user/me')
-      .set('Authorization', 'Bearer ' + accToken)
-      .expect(200);
-
     // Do not hardcode server port and address, square brackets are used for IPv6
     StatusSocketClient = io(
       `ws://[${restApp.getHttpServer().address().address}]:10051/status`,
@@ -173,8 +127,6 @@ describe('Socket', () => {
     StatusSocketClient.disconnect();
     ChatSocketClient.removeAllListeners();
     ChatSocketClient.disconnect();
-    // await datasource.getRepository(Token).delete({ ownerId: 10000 });
-    // await datasource.getRepository(User).delete({ id: 10000 });
   });
 
   afterAll(async () => {
@@ -194,22 +146,135 @@ describe('Socket', () => {
   });
 
   describe('Chat', () => {
+    /**
+     * owner
+     * */
+    let user10000: User;
+    /**
+     * admin
+     * */
+    let user10001: User;
+    /**
+     * joinedUser
+     */
+    let user10002: User;
+
+    /**
+     * joinedUser
+     */
+    let user10003: User;
+    let groupChat10000: GroupChat;
+
+    beforeEach(async () => {
+      console.log('beforeEach');
+      user10000 = await datasource
+        .getRepository(User)
+        .save(userFactory.createUser(10000));
+
+      user10001 = await datasource
+        .getRepository(User)
+        .save(userFactory.createUser(10001));
+      user10002 = await datasource
+        .getRepository(User)
+        .save(userFactory.createUser(10002));
+      user10003 = await datasource
+        .getRepository(User)
+        .save(userFactory.createUser(10003));
+      groupChat10000 = await datasource
+        .getRepository(GroupChat)
+        .save(chatFactory.createPubChat(user10000.id, 10001));
+      // Setup
+      //10000번 유저로 로그인
+      const res = await request(restApp.getHttpServer())
+        .get('/api/fakeauth/login')
+        .query({
+          id: user10000.id,
+        });
+
+      accToken = res.headers['location'].split('accessToken=')[1];
+
+      //login
+      await request(restApp.getHttpServer())
+        .get('/api/user/me')
+        .set('Authorization', 'Bearer ' + accToken)
+        .expect(200);
+      await datasource
+        .createQueryBuilder(GroupChat, 'groupChat')
+        .relation('admin')
+        .of(groupChat10000)
+        .add(user10001);
+
+      await datasource
+        .createQueryBuilder(GroupChat, 'groupChat')
+        .relation('joinedUser')
+        .of(groupChat10000)
+        .add([user10002, user10003]);
+    });
+
+    afterEach(async () => {
+      console.log('afterEach');
+      ChatSocketClient.removeAllListeners();
+      ChatSocketClient.disconnect();
+      await datasource
+        .createQueryBuilder(GroupChat, 'groupChat')
+        .relation('admin')
+        .of(groupChat10000)
+        .remove(user10001);
+
+      await datasource
+        .createQueryBuilder(GroupChat, 'groupChat')
+        .relation('joinedUser')
+        .of(groupChat10000)
+        .remove([user10002, user10003]);
+
+      await datasource
+        .createQueryBuilder(GroupChat, 'groupChat')
+        .delete()
+        .execute();
+
+      await datasource.getRepository(Token).delete({
+        ownerId: In([user10000.id, user10001.id, user10002.id, user10003.id]),
+      });
+      await datasource.createQueryBuilder(User, 'user').delete().execute();
+    });
+
     it('kick', (done) => {
       ChatSocketClient.connect();
       ChatSocketClient.on('kick-user', (data) => {
-        console.log(data);
+        expect(data.groupChatId).toBe(groupChat10000.groupChatId);
+        expect(data.userId).toBe(user10002.id);
         done();
       });
       ChatSocketClient.on('error', (data) => {
-        console.log(data);
+        expect(data).toBeNull();
         done();
       });
       const kickUserDto = new KickUserDto();
-      kickUserDto.requestUserId = 10000;
-      kickUserDto.kickUserId = 10002;
+      kickUserDto.requestUserId = user10000.id;
+      kickUserDto.kickUserId = user10002.id;
       kickUserDto.groupChatId = groupChat10000.groupChatId;
       console.log(kickUserDto);
       ChatSocketClient.emit('kick-user', kickUserDto);
     });
+
+    it('ban owner -> joinedUser', (done) => {
+      ChatSocketClient.connect();
+      ChatSocketClient.on('ban-user', (data) => {
+        expect(data.groupChatId).toBe(groupChat10000.groupChatId);
+        expect(data.userId).toBe(user10002.id);
+        done();
+      });
+      ChatSocketClient.on('error', (data) => {
+        expect(data).toBeNull();
+        done();
+      });
+      const dto = new BanDto();
+
+      dto.groupChatId = groupChat10000.groupChatId;
+      dto.userId = user10000.id;
+      dto.bannedId = user10002.id;
+      ChatSocketClient.emit('ban-user', dto);
+    });
+    // it('unban', (done) => {});
   });
 });
