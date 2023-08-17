e7f1e62620fbe542dc70606848c8f517ae091bd8
myukang <myunghwan0421@gmail.com>
2023-08-18 03:22:37 +0900

    :sparkles: feat: getMuteOffset

--- src/restapi/chat/chat.service.ts
+++ src/restapi/chat/chat.service.ts
@@ -32,7 +32,6 @@ import { UnBanDto } from './request/unBan.dto';
 import { KickUserDto } from './request/kickUser.dto';
 import { GetBanMuteListDto } from './request/getBanMuteList.dto';
 import { GetMuteOffsetDto } from './request/getMuteOffset.dto';
-import { BanMuteList } from './response/BanMuteList.dto';
 
 @Injectable()
 export class ChatService {
@@ -42,9 +41,6 @@ export class ChatService {
 
     @InjectRepository(BlockUserList)
     private readonly blockUserListRepository: Repository<BlockUserList>,
-
-    @InjectRepository(MutedUserJoin)
-    private readonly mutedUserJoinRepository: Repository<MutedUserJoin>,
   ) {}
 
   async getGroupChatList() {
@@ -1029,7 +1025,8 @@ export class ChatService {
           .update()
           .set({
             curParticipants: () => 'curParticipants - 1',
-          });
+          })
+          .execute();
 
         return {
           groupChatId: groupChatId,
@@ -1089,5 +1086,43 @@ export class ChatService {
     );
   }
 
-  async getMuteOffset(groupChatId: number, dto: GetMuteOffsetDto) {}
+  async getMuteOffset(groupChatId: number, dto: GetMuteOffsetDto) {
+    return await this.groupChatRepository.manager.transaction(
+      async (manager: EntityManager) => {
+        //UTC 기준으로 현재 시간을 가져옴
+        const muteDue = await manager.getRepository(MutedUserJoin).findOne({
+          where: {
+            mutedUserId: dto.userId,
+            mutedGroupId: groupChatId,
+          },
+        });
+        //muteDue가 없는 경우
+        if (!muteDue) {
+          return {
+            groupChatId: groupChatId,
+            muteFor: 0,
+          };
+        }
+
+        //muteDue가 있는 경우
+        const now = new Date();
+        const muteDueDate = new Date(muteDue.muteDue);
+        const muteFor = muteDueDate.getTime() - now.getTime();
+
+        //muteDue가 지난 경우
+        //muteDue를 삭제
+        if (muteFor < 0) {
+          await manager.getRepository(MutedUserJoin).delete({
+            mutedUserId: dto.userId,
+            mutedGroupId: groupChatId,
+          });
+        }
+
+        return {
+          groupChatId: groupChatId,
+          muteFor: muteFor,
+        };
+      },
+    );
+  }
 }
