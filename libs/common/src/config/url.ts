e8539b08074f0ed424fc81860752d5a35a5b6277
koreanddinghwan <myunghwan0421@gmail.com>
2023-10-03 16:07:57 +0900

    fix: url

--- libs/common/src/config/url.ts
+++ libs/common/src/config/url.ts
@@ -1,10 +1,7 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('url', () => ({
-  frontHost:
-    process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test'
-      ? 'http://localhost'
-      : process.env.REACT_APP_HOST,
+  frontHost: process.env.REACT_APP_HOST,
   frontPort: process.env.REACT_PORT,
 
   nestServerUrl:
